8954397179fe24fb1720c134f3bec75dc2798247
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-13 17:31:10 +0300

    completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CategoryFilter({ categories, onClick }) {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleClick = (category) => {
+    setSelectedCategory(category);
+    onClick(category);
+  };
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {categories.map((category, index) => (
-        <button key={index} onClick={() => onClick(category)}>
+        <button 
+          key={index} 
+          onClick={() => handleClick(category)}
+          className={category === selectedCategory ? 'selected' : ''}
+        >
           {category}
         </button>
       ))}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-function Task({ text, category }) {
+function Task({ text, category, onDelete }) {
+  const handleDelete = useCallback(() => {
+    onDelete(text);
+  }, [onDelete, text]);
   return (
     <div className="task">
       <div className="label">{category}</div>
       <div className="text">{text}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={handleDelete}>X</button>
     </div>
   );
 }
